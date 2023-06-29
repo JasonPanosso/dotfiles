31e1e64319ce0321ae3feb44a94755427bce7517
JasonPanosso <jasonpanosso@protonmail.com>
2023-06-29 16:56:28 -0400

    update restore-i3 script

--- .restore-i3.sh
+++ .restore-i3.sh
@@ -4,6 +4,9 @@ sleep 0.2
 i3-msg [workspace=4] move workspace to output DP-4
 sleep 0.2
 
+i3-resurrect restore -w 10
+sleep 0.2
+
 i3-resurrect restore -w 2
 sleep 0.2
 i3-msg [workspace=2] move workspace to output DP-2
@@ -13,19 +16,3 @@ i3-resurrect restore -w 3
 sleep 0.2
 
 i3-resurrect restore -w 1
-sleep 0.2
-
-i3-resurrect restore -w 5
-sleep 0.2
-i3-msg [workspace=5] move workspace to output DP-4
-sleep 0.2
-
-i3-resurrect restore -w 8
-sleep 0.2
-
-i3-resurrect restore -w 9
-sleep 0.2
-
-i3-resurrect restore -w 10
-sleep 0.2
-
