1fbe3652c01ce1319b9ca5dfa8247b7ed9a822b8
JasonPanosso <jasonpanosso@protonmail.com>
2023-08-09 15:39:57 -0400

    fix i3 stuff, change kb layout

--- .restore-i3.sh
+++ .restore-i3.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 i3-resurrect restore -w 4
 sleep 0.2
-i3-msg [workspace=4] move workspace to output DP-4
-sleep 0.2
 
 i3-resurrect restore -w 10
 sleep 0.2
 
 i3-resurrect restore -w 2
 sleep 0.2
-i3-msg [workspace=2] move workspace to output DP-2
+i3-msg move workspace to output next
 sleep 0.2
 
 i3-resurrect restore -w 3
